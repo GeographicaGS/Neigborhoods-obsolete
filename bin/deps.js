e417baba3f460ac563e44c369bb2bdcfba9e3787
Javier Aragón <javier.aragon@geographica.gs>
2015-05-28 12:12:32 +0200

    bug al compilar en producción

--- bin/deps.js
+++ bin/deps.js
@@ -1,6 +1,6 @@
 var deps = [
-    "lib/jquery-2.1.3.js",
-    "lib/leaflet-0.7.3/leaflet.js",
+    "../lib/jquery-2.1.3.js",
+    "../lib/leaflet-0.7.3/leaflet.js",
     "global.js",
     "family.js",
     "indicator.js"
