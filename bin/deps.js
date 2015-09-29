1859186b9f50de2192555244f8129a28ba7626a4
Javier Aragón <javier.aragon@geographica.gs>
2015-09-29 14:57:31 +0200

    bugA2

--- bin/deps.js
+++ bin/deps.js
@@ -1,6 +1,8 @@
 var deps = [
     "../lib/jquery-2.1.3.js",
     "../lib/leaflet-0.7.3/leaflet.js",
+    "../lib/fancybox/jquery.fancybox.pack.js",
+    "../lib/fancybox/helpers/jquery.fancybox-thumbs.js",
     "global.js",
     "family.js",
     "indicator.js",
